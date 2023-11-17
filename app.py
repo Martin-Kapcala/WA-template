87b18d8d3178d7444635b35e2b8467501cf47f52
Martin-Kapcala <martin.kapcala@vitkovickastredni.cz>
2023-11-17 11:43:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+class Upload(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(50))
+    data = db.Column(db.LargeBinary)
+
 
 #Hrátky s uživateli - vytvoření testovacích instancí objektu Uzivatel    
 martin = User("Martin", None)
